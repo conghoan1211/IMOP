fa2514e0d8081da35373f4879425e78e31f0e0a4
dinamok59 <dinamok59@gmail.com>
2024-10-07 02:05:28 +0700

    Remove debug files and folders from tracking

--- zaloclone test/obj/Debug/net6.0/zaloclone.AssemblyInfo.cs
+++ zaloclone test/obj/Debug/net6.0/zaloclone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zaloclone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d7222481eed9395072c27ea3ea065fc771a873")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b2ad3359caef0b5847e9b020dd7a74aeb693e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("zaloclone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zaloclone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
