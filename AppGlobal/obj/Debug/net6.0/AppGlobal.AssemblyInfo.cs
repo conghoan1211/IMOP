be1e1fbc8735a7f3db368f18743e05672b3a61b4
dinamok59 <dinamok59@gmail.com>
2024-10-07 02:44:09 +0700

    Sonct | debug

--- AppGlobal/obj/Debug/net6.0/AppGlobal.AssemblyInfo.cs
+++ AppGlobal/obj/Debug/net6.0/AppGlobal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppGlobal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa2514e0d8081da35373f4879425e78e31f0e0a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5091b931a2edeb475a92ef0b02e35e173fc80b50")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppGlobal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppGlobal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- zaloclone test/obj/Release/net6.0/zaloclone.AssemblyInfo.cs
+++ zaloclone test/obj/Release/net6.0/zaloclone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zaloclone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc1a226876addcc9f67c7e72d8bb38a74bc356d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5091b931a2edeb475a92ef0b02e35e173fc80b50")]
 [assembly: System.Reflection.AssemblyProductAttribute("zaloclone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zaloclone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
