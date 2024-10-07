9b99dbac4aa2c910175cdf3ad28ce2efb67b9df2
dinamok59 <dinamok59@gmail.com>
2024-10-08 00:35:49 +0700

    SonCT | debug

--- AppGlobal/obj/Release/net6.0/AppGlobal.AssemblyInfo.cs
+++ AppGlobal/obj/Release/net6.0/AppGlobal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppGlobal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5091b931a2edeb475a92ef0b02e35e173fc80b50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d3d29f45a209d53b96588fd48dd4b8edcdab79d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppGlobal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppGlobal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
