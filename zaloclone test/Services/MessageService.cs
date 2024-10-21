2d5629d1736168c282660b6e4739634cfdc41f30
Nam Nguyen <153477114+AnemosNguyen@users.noreply.github.com>
2024-10-21 21:53:16 +0700

    add message service

---
+++ zaloclone test/Services/MessageService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using zaloclone_test.Models;
+
+namespace zaloclone_test.Services
+{
+    public interface IMessageService
+    {
+        public Task<List<Message>> GetLastMessages(string ownerId);
+    }
+    public class MessageService : IMessageService
+    {
+        private readonly Zalo_CloneContext _context;
+        public MessageService(Zalo_CloneContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Message>> GetLastMessages(string ownerId)
+        {
+            throw new NotImplementedException();
+            List<Message> messages = await _context.Messages.Where(message => message.SenderId == ownerId).ToListAsync();
+        }
+    }
+}
