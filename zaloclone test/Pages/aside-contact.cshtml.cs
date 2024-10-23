0130b4d14b9a9a94ff27a18dba6859a38b8d2de8
DESKTOP-HQGPGS8\Dung <trungdung23102003@gmail.com>
2024-10-23 11:55:08 +0700

    Fix the search function

--- zaloclone test/Pages/aside-contact.cshtml.cs
+++ zaloclone test/Pages/aside-contact.cshtml.cs
@@ -66,7 +66,12 @@ namespace Server.Pages
             }
         }
 
-        public async Task<IActionResult> OnPostDeleteFriendAsync([FromBody] string friendId)
+        public class DeleteFriendModel
+        {
+            public string FriendId { get; set; }
+        }
+
+        public async Task<IActionResult> OnPostDeleteFriendAsync([FromBody] DeleteFriendModel model)
         {
             string msg = _jwtAuthen.ParseCurrentToken(User, out UserToken userToken);
             if (msg.Length > 0)
@@ -77,7 +82,7 @@ namespace Server.Pages
 
             try
             {
-                var result = await _asideContactService.DeleteFriend(UserToken.UserID.ToString(), friendId);
+                var result = await _asideContactService.DeleteFriend(UserToken.UserID.ToString(), model.FriendId);
                 return new JsonResult(result);
             }
             catch (Exception ex)
